b7167a641fd8e0bdbde3cda95db69b5cd6a2268b
vimal-1963 <vmathew7@my.centennialcollege.ca>
2023-11-28 12:49:09 -0500

    first commit

---
+++ Helper.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.S3;
+using System.Configuration;
+
+
+namespace Api_Project
+{
+    public class Helper
+    {
+        public readonly static IAmazonS3 s3Client;
+        public readonly static AmazonDynamoDBClient dynamoDBClient;
+        static Helper()
+        {
+            s3Client = GetS3Client();
+            dynamoDBClient = GetDynamoDBClient();
+        }
+
+        private static IAmazonS3 GetS3Client()
+        {
+            string accessKey = ConfigurationManager.AppSettings["accessId"];
+            string awsSecretKey = ConfigurationManager.AppSettings["password"];
+            return new AmazonS3Client(accessKey, awsSecretKey, RegionEndpoint.CACentral1);
+        }
+
+        private static AmazonDynamoDBClient GetDynamoDBClient()
+        {
+            string accessKey = ConfigurationManager.AppSettings["accessId"];
+            string awsSecretKey = ConfigurationManager.AppSettings["password"];
+            return new AmazonDynamoDBClient(accessKey, awsSecretKey, RegionEndpoint.CACentral1);
+        }
+    }
+}
